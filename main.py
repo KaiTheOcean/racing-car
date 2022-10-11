ae4b7f5b4a71c5aa164f739ceb7fa3aa1bb72876
KaiTheOcean <kaizhangd@gmail.com>
2022-10-10 22:36:33 -0600

    addded stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame import *
+from pygame.locals import *
 import sys
 import player
 import cop
@@ -7,7 +7,9 @@ import background
 
 # Set the pygame window size
 pygame.init()
-screen_window = pygame.display.set_mode((1000, 400))
+width = 1000
+height = 400
+screen_window = pygame.display.set_mode((width, height))
 bg_color = (0, 0, 0)
 
 # Set the caption name
@@ -20,15 +22,22 @@ screen_rect = screen_window.get_rect()
 # Create player, all other enemy cars
 player = player.Player()
 cop = cop.Cop()
-background = background.Background()
+# background = background.Background()
+background_image = pygame.image.load("images/road.png")
+background_image = pygame.transform.scale(background_image, (width, height))
 
 i = 0
 # Run the game
 running = True 
 while running: 
-    screen_window.blit(background.image, (0, -50))
-    screen_window.blit(player.image, player.rect)
-    screen_window.blit(cop.image, cop.rect)
+    screen_window.blit(background_image, (i, 0)) # Display background picture
+    screen_window.blit(background_image, (width+1, 0))
+    if (i == -width):
+        screen_window.blit(background_image, (width+i, 0))
+        i = 0
+    i -= 1
+    screen_window.blit(player.image, player.rect) # Display player car
+    screen_window.blit(cop.image, cop.rect) # Display cop car
     player.control_player()
     cop.move()
     pygame.display.flip()
